2250ba87f94afe7b2bb716e6a5f67d1f7a91d00c
Mikael Icaro <mikael.ikaro9@gmail.com>
2025-02-24 21:14:58 +0000

    lib.rs

---
+++ novo-repositorio/aula1/reverse_sring/src/lib.rs
@@ -0,0 +1,53 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+//Criando uma função pública 
+
+pub fn reverse(input: &str) -> String {
+    
+    input.chars().rev().collect()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
